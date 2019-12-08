27adb9b201a1fae7956b20ace19e714b2d3b84f9
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-12-08 12:30:45 +0900

    test: Add not to be case

--- __tests__/get-url.test.ts
+++ __tests__/get-url.test.ts
@@ -7,8 +7,10 @@ describe('getURL()', () => {
     const urlLinux: string = `${baseURL}-unknown-linux-gnu.tar.gz`;
     const urlMacOS: string = `${baseURL}-apple-darwin.tar.gz`;
     const urlWindows: string = `${baseURL}-pc-windows-msvc.zip`;
+    const urlMyOS: string = `${baseURL}-my-os.tar.gz`;
     expect(getURL('unknown-linux-gnu', '0.3.5')).toBe(urlLinux);
     expect(getURL('apple-darwin', '0.3.5')).toBe(urlMacOS);
     expect(getURL('pc-windows-msvc', '0.3.5')).toBe(urlWindows);
+    expect(getURL('unknown-linux-gnu', '0.3.5')).not.toBe(urlMyOS);
   });
 });
