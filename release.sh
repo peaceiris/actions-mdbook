314c9a4ecbbe1251e1bb17335ea141e153d2f863
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-01-01 17:48:04 +0900

    ci: Fix release workflow

--- release.sh
+++ release.sh
@@ -3,37 +3,26 @@
 # fail on unset variables and command errors
 set -eu -o pipefail # -x: is for debugging
 
-BASE_BRANCH="$(git branch --show-current)"
-
-echo "Base branch is ${BASE_BRANCH}, continue? (y/n)"
-read -r res
-if [ "${res}" = "n" ]; then
-  echo "Stop script"
-  exit 0
-fi
-
-RELEASE_TYPE_LIST="major minor patch premajor preminor prepatch prerelease"
-
-if command -v fzf; then
-  RELEASE_TYPE=$(echo "${RELEASE_TYPE_LIST}" | tr ' ' '\n' | fzf --layout=reverse)
-else
-  select sel in ${RELEASE_TYPE_LIST}; do
-    RELEASE_TYPE="${sel}"
-    break
-  done
-fi
-
-echo "Create ${RELEASE_TYPE} release"
-RELEASE_BRANCH="release-${RELEASE_TYPE}"
-echo "Release branch is ${RELEASE_BRANCH}"
-
-git fetch origin
-git push origin "${BASE_BRANCH}"
-git pull origin "${BASE_BRANCH}"
-
-if ! git branch "${RELEASE_BRANCH}"; then
+if [ "$(git branch --show-current)" != "master" ]; then
+  echo "Current branch is not master" 1>&2
   exit 1
 fi
 
-git checkout "${RELEASE_BRANCH}"
-git push origin "${RELEASE_BRANCH}"
+git fetch origin
+git pull origin master
+git pull origin --tags
+
+mkdir ./lib
+npm run build
+git add ./lib/index.js
+git commit -m "chore(release): Add build assets"
+
+npm run release --preset eslint
+
+git rm ./lib/index.js
+rm -rf ./lib
+git commit -m "chore(release): Remove build assets"
+
+TAG_NAME="v$(jq -r '.version' ./package.json)"
+git push origin master
+git push origin "${TAG_NAME}"
