6265838f38300de36e66fa3ce6c28d582f0044ff
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-01-07 03:40:43 +0000

    test: Rename test name

--- __tests__/get-os.test.ts
+++ __tests__/get-os.test.ts
@@ -1,13 +1,13 @@
 import {getOS} from '../src/get-os';
 
-describe('getOS', () => {
-  test('test', () => {
+describe('getOS()', () => {
+  test('should return expected OS name', () => {
     expect(getOS('linux')).toBe('unknown-linux-gnu');
     expect(getOS('darwin')).toBe('apple-darwin');
     expect(getOS('win32')).toBe('pc-windows-msvc');
   });
 
-  test('test exception', () => {
+  test('should return exception', () => {
     expect(() => {
       getOS('centos');
     }).toThrowError('centos is not supported');
