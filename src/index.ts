0d19991235fbfcc105f6bd13673665bce06005de
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2020-06-22 01:59:58 +0900

    fix: Wrap entrypoint by async to handle exceptions correctly (#246)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import {run} from './main';
+import * as core from '@actions/core';
+import * as main from './main';
 
-run();
+(async (): Promise<void> => {
+  try {
+    await main.run();
+  } catch (e) {
+    core.setFailed(`Action failed with error ${e.message}`);
+  }
+})();
